f37f3352c5f8d4cc6b98fb368cbb465dbe6a3e76
Phil Spitzer <thespitzer@gmail.com>
2014-04-21 16:31:18 -0700

    more oauth shit

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['Kp6nwP7uYB6n87RbYsAKiHIbI'], ENV['oHicY35eOGuRA99RFWn8vz82TrBFUb9oBLEboRPKSUmhUeQASx']
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Askaway::Application.routes.draw do
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
+  match 'auth/twitter/callback', to: 'sessions#create', via: 'get'
+  match 'auth/failure', to: redirect('/'), via: 'get'
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
