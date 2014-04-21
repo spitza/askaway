b4f0867f6b2947e59c3c358a7bb62becdbab5d83
Phil Spitzer <thespitzer@gmail.com>
2014-04-20 21:07:35 -0700

    finish layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,21 +5,21 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'askaway'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('askaway')
     end
   end
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
   end
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
   end
