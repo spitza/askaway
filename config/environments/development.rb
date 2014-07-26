1400e953acacb43897f96ed16d28150ce06f4895
Phil Spitzer <thespitzer@gmail.com>
2014-07-26 16:36:52 -0700

    sticky bullshit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,5 +26,5 @@ Askaway::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 end
