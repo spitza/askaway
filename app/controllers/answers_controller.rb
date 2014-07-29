505a478d110aa27e50bb55f4312cfdf47bcdfced
Phil Spitzer <thespitzer@gmail.com>
2014-07-29 12:32:57 -0700

    delete answers from within question controller woooooooo

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,6 @@
 class AnswersController < ApplicationController
   before_action :current_user, only: [:create]
+  before_action :correct_user, only: :destroy
     
   def create
     @answer = Answer.new(answer_params)
@@ -13,10 +14,24 @@ class AnswersController < ApplicationController
     end
   end
   
+  def destroy
+    @answer = Answer.find(params[:id])
+    @question = @answer.question
+    @answer.destroy
+    redirect_to @question
+  end
+  
   private
 
     def answer_params
       params.require(:answer).permit(:content, :question_id)
+    end
+    
+    def correct_user
+      @answer = Answer.find(params[:id])
+      unless @answer.question.conversation.user == current_user
+        redirect_to root_url
+      end
     end
     
 end
